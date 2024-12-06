79e7927bb0746a2b0bb182beeee35bd652af958d
crespiramiro <crespiramiro@outlook.com>
2024-12-06 00:16:02 -0300

    CORS config

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, Blueprint
 from routes.routes import tasks_bp
 from db.db import init_db
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
     
 app.register_blueprint(tasks_bp, url_prefix='/tasks')
 
